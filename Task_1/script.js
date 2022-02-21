ee1e76c521db1b3d037b51ef259c3959f1d6cc5d
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-02-21 19:14:18 +0300

    reverted

--- Task_1/script.js
+++ Task_1/script.js
@@ -9,3 +9,4 @@ function showOwnProperties(obj) {
     }
 }
 
+
--- Task_3/script.js
+++ Task_3/script.js
@@ -1,8 +1,9 @@
 /* Напишите функцию, которая создает пустой объект, но без прототипа. */
-
+// 
 function createObj() {
-	return Object.create(null);
+	return Object.create(null); 
 }
+// Создаем пустой объект
 sampleObj = createObj();
 
 console.log(Object.getPrototypeOf(sampleObj));
