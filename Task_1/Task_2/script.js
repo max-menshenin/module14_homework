e16405b83fba5b0dbb0e5d5f3b4506840e9207fd
Postmind <53375202+max-menshenin@users.noreply.github.com>
2022-02-23 22:49:11 +0300

    Delete Task_1/Task_2 directory

--- Task_1/Task_2/script.js
+++
@@ -1,7 +0,0 @@
-/*Напишите функцию, которая принимает в качестве аргументов строку и объект, а затем проверяет, есть ли у переданного объекта свойство с данным именем. Функция должна возвращать true или false.*/
-
-const obj = {a: 'hat'};
-let str = 'a';
-const hasFunc = (str, obj) => obj.hasOwnProperty(str);
-console.log(obj.hasOwnProperty(str));
-   
